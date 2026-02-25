2984faed867b43733ec8905aecc534502896435c
Felipe Thomaz <fthomaz100@gmail.com>
2026-02-25 16:52:48 -0300

    Add files via upload

---
+++ src/main/java/com/senai/conta_bancaria/domain/entity/Cliente.java
@@ -0,0 +1,5 @@
+package com.senai.conta_bancaria.domain.entity;
+
+public class Cliente extends Usuario {
+    private Conta contaa;
+}
---
+++ src/main/java/com/senai/conta_bancaria/interface_ui/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package com.senai.conta_bancaria.interface_ui.controller;
+
+import com.senai.conta_bancaria.application.service.UsuarioService;
+import com.senai.conta_bancaria.domain.entity.Usuario;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    UsuarioService usuarioService;
+
+    @PostMapping
+    public Usuario cadastrarUsuario(@RequestBody Usuario usuario) {
+
+        usuarioService.cadastrarUsuario(usuario);
+        return usuario;
+    }
+
+
+}
